8542ea20a19f37f76143baebe4c6dd45a13bdaea
Phoenix Nemo <mrphoenixlzx@gmail.com>
2013-08-21 21:01:12 +0800

    removed dump config files

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    pkginfopath: "/home/www/repo-arm", // pkginfo.db file path, without the last slash "/". Currently not usable.
-    downloadurl: "http://repo-arm-download.archlinuxcn.org"  // package download url, for returning package download links.
-}
