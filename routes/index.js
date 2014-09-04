d636962e2fd7a575ed1884089afd54e198d27078
Phoenix Nemo <i@phoenixlzx.com>
2014-09-04 11:57:54 +0800

    typo

--- routes/index.js
+++ routes/index.js
@@ -35,7 +35,7 @@ router.get('/search', function(req, res) {
         pkgs.push(row.pkgrepo + "|" + row.pkgname + "|" + row.pkgarch + "|" + row.pkgver + "|"
             + config.downloadurl + row.pkgrepo + "/os/" + req.query.arch
             + row.filename.substr(row.filename.lastIndexOf("/")) + "|"
-            + row.pkgver.slice(row.pkgver.lastIndexOf("-" + 1))
+            + row.pkgver.slice(row.pkgver.lastIndexOf("-") + 1)
             + "\n");
         pkgs = pkgs.naturalSort().reverse();
         /*
@@ -80,7 +80,7 @@ router.post('/exact', function(req, res) {
         pkgs.push(row.pkgrepo + "|" + row.pkgname + "|" + row.pkgarch + "|" + row.pkgver + "|"
             + config.downloadurl + row.pkgrepo + "/os/" + pkgarch
             + row.filename.substr(row.filename.lastIndexOf("/")) + "|"
-	    + row.pkgver.slice(row.pkgver.lastIndexOf("-" + 1))
+	    + row.pkgver.slice(row.pkgver.lastIndexOf("-") + 1)
             + "\n");
         pkgs = pkgs.naturalSort().reverse();
         /*
@@ -120,7 +120,7 @@ router.post('/find', function(req, res) {
         pkgs.push(row.pkgrepo + "|" + row.pkgname + "|" + row.pkgarch + "|" + row.pkgver + "|"
             + config.downloadurl + row.pkgrepo + "/os/" + pkgarch
             + row.filename.substr(row.filename.lastIndexOf("/")) + "|"
-	    + row.pkgver.slice(row.pkgver.lastIndexOf("-" + 1))
+	    + row.pkgver.slice(row.pkgver.lastIndexOf("-") + 1)
             + "\n");
         pkgs = pkgs.naturalSort().reverse();
         /*
